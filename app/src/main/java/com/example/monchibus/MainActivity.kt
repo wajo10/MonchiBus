8a5e6d112821a3f424a22d6d1ac4b18bd1a7f08b
Wajib Zaglul <wajib10@gmail.com>
2020-11-14 14:33:03 -0600

    [add] mapview

--- app/src/main/java/com/example/monchibus/MainActivity.kt
+++ app/src/main/java/com/example/monchibus/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.monchibus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        login.setOnClickListener{
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
